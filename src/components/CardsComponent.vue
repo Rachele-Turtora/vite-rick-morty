9df89bc8ff2f491a7abc0c4f6673a8917b87be49
Rachele-Turtora <racheleturtora21@gmail.com>
2024-06-03 16:41:11 +0200

    [FIX] details

--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -43,7 +43,9 @@ img {
     height: 220px;
     width: 220px;
     border-radius: 50%;
-    padding: 10px 0;
+    margin: 15px 0;
+    object-fit: cover;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 
 .description {
